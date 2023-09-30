8e8808118e2f3b9e40e0675c379b1dadc242952a
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2023-09-30 20:30:51 +0900

    rename package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 import CompilerPluginSupport
 
 let package = Package(
-    name: "DependenciesMacro",
+    name: "swift-dependencies-macro",
     platforms: [
         .macOS(.v10_15),
         .iOS(.v13),
